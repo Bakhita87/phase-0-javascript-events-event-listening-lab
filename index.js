c8ac3958ce6df2d241eb49f37feed77c40736038
Bakhita87 <bakhitaotieno@gmail.com>
2023-06-24 01:22:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 function addingEventListener() {
+    
+
+const input = document.getElementById('input');
+
+
+
+input.addEventListener('click', clickAlert);
+
+
 }
+
+function clickAlert() {
+    alert('I was clicked!');
+  }
+  
+
+
+addingEventListener()
